44b427a4c3bb90ee76864b1963370124964e706e
Cole Grim <colegrim@gmail.com>
2015-02-10 19:47:09 -0800

    fixed bad colors that don't display in consoles well and added README.

--- com/colegrim/UI.java
+++ com/colegrim/UI.java
@@ -29,9 +29,9 @@ public class UI implements Runnable{
     }
 
     public enum AsciiColor{
-        RESET("\u001B[0m"), BLACK("\u001B[30m"), RED("\u001B[31m"), GREEN("\u001B[32m"),
-        YELLOW("\u001B[33m"), BLUE("\u001B[34m"), PURPLE("\u001B[35m"), CYAN("\u001B[36m"),
-        WHITE("\u001B[37m");
+        RESET("\u001B[0m"), RED("\u001B[31m"), GREEN("\u001B[32m"),
+        YELLOW("\u001B[33m"), BLUE("\u001B[34m"), PURPLE("\u001B[35m"),
+        CYAN("\u001B[36m");
 
         final String code;
 
